e03c2b6bd29cd2fe7a383853d2a77ffa13c693ea
hayoung0Lee <hayoung0.lee@gmail.com>
2021-03-04 14:18:00 +0900

    Add Gatsby Client

---
+++ client/READEME.md
@@ -0,0 +1,12 @@
+# Gatsby Client
+
+1. Making Gatsby Project
+
+```
+gatsby new client
+
+```
+
+## References
+
+[The Back End: Using Gatsby.js and Node.js for Static Site Updates](https://www.toptal.com/nodejs/gatsby-js-node-js-static-site-generator-pt-1)
---
+++ client/src/pages/index.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+// import { Link } from "gatsby"
+// import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+  </Layout>
+)
+
+export default IndexPage
